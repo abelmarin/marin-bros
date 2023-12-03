bd4b52cb07f951aa3a756a0ccfef42605bddfdeb
Abel Marin <marinabe1416@gmail.com>
2023-12-02 23:23:12 -0500

    Update 404 style

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,11 @@
 export default function Page() {
-  return <main>404</main>
+  return (
+    <main>
+      <section className="px-4">
+        <div>
+          <h1>404 Not Found</h1>
+        </div>
+      </section>
+    </main>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document"
 import Script from "next/script"
-Script
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script src={`https://www.googletagmanager.com/gtag/js?id=G-7PDQT8H6S9`} strategy="afterInteractive" />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-7PDQT8H6S9" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
                 window.dataLayer = window.dataLayer || [];
